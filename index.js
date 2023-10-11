f300c28145c520496d005a8c23e65a196d897aaf
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2023-10-11 22:00:24 +1100

    ✅ Pass getVowelPosition expected tests

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ export function getOffset(vowelsLength) {
     return offset;
 }
 
+export function getVowelPosition(vowel, index, array) {
+    const firstVowelPositions = { i:0, e:1, a:2, o:3, u:4 };
+    const adjacentVowelPositions = {
+        i: { e:0, a:1, o:2, u:3 },
+        e: { i:0, a:1, o:2, u:3 },
+        a: { i:0, e:1, o:2, u:3 },
+        o: { i:0, e:1, a:2, u:3 },
+        u: { i:0, e:1, a:2, o:3 },
+    };
+    if (index === 0) {
+        return firstVowelPositions[vowel];
+    }
+    const vowelPosition = adjacentVowelPositions[array[index-1]][vowel];
+    if (index === (array.length - 1)) {
+        return vowelPosition + 1;
+    }
+    return vowelPosition;
+}
+
 export default function parseEberbanNumber(eberbanNumber) {
     return ({
         ti: 0,
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import parseEberbanNumber, { getOffset } from ".";
+import parseEberbanNumber, { getOffset, getVowelPosition } from ".";
 
 describe("expected input", () => {
     describe("Get offset", () => {
@@ -12,6 +12,21 @@ describe("expected input", () => {
         ])("%d-digit number is offset by %d", (vowelsLength, offset) => {
             expect(getOffset(vowelsLength)).toBe(offset);
         })
+    });
+    describe("Get vowel position", () => {
+        test.each([
+            { array: ["i"], index: 0, output: 0 },
+            { array: ["e"], index: 0, output: 1 },
+            { array: ["a"], index: 0, output: 2 },
+            { array: ["o"], index: 0, output: 3 },
+            { array: ["u"], index: 0, output: 4 },
+            { array: ["i","e"],  index: 1, output: 1 },
+            { array: ["e", "o"], index: 0, output: 1 },
+            { array: ["a", "u", "o"], index: 1, output: 3 },
+            { array: ["u", "i", "e"], index: 1, output: 0 },
+        ])("index $index of $array -> $output", ({ array, index, output }) => {
+            expect(getVowelPosition(array[index], index, array)).toBe(output);
+        });
     });
     describe("1-digit numbers", () => {
         test.each([
